83307eb5101fe135d459fa012d74c09aec708ab3
Akash Sare <akashsare03@gmail.com>
2025-05-27 13:35:10 +0530

    Added Day 2: Python data structures (list, tuple, dictionary)

---
+++ Week1_Python_Core/day2_data_structures.py
@@ -0,0 +1,18 @@
+
+my_list = [1,2,3,4]
+print("List :", my_list)
+
+my_list.append(5)
+print("Updated List: ", my_list)
+
+my_list.remove(3)
+print("After removing 3 :", my_list)
+
+my_tuple = (10,20,30)
+print("Tuple: ", my_tuple)
+
+my_dict = {"Name":"Akash", "Age":25}
+print("Dictonary:", my_dict)
+
+my_dict["City"]="Pune"
+print("Updated Dictonary:" , my_dict)
